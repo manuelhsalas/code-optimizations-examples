7afae5f42c83b6071aa6f7dc14c4172961e1eed8
Manu Herrera <Manuel.Herrera2@Honeywell.com>
2019-08-12 11:34:28 -0500

    - a basic project with the implementation of 3 ways for copying a large block of memory. This example takes advantage of optimized arm copy library of unaligned memory.

---
+++ fast-mem-copy/main.c
@@ -0,0 +1,51 @@
+#include <string.h>
+
+typedef unsigned int U32;
+
+#define ARRAY_SIZE  (1000)
+
+U32 a[ARRAY_SIZE];
+U32 ac[ARRAY_SIZE];
+
+struct sMemBlock
+{
+      U32 b[ARRAY_SIZE];  
+} memblock1, memblock2;
+
+void funcion(void);
+void funcion2(void);
+void funcion3(void);
+
+int main()
+{
+  funcion();
+  
+  funcion2();
+  
+  funcion3();
+  
+  return 0;
+}
+
+void funcion(void)
+{
+    U32 i;
+
+    /* inefficient */
+    for(i = 0; i < ARRAY_SIZE; i++)
+    {
+        ac[i] = a[i];
+    }
+}
+
+void funcion2(void)
+{
+    /* optimal copying */
+    memblock2 = memblock1;
+}
+
+void funcion3(void)
+{
+    /* inefficient */
+    memcpy(ac, a, sizeof(a));
+}
